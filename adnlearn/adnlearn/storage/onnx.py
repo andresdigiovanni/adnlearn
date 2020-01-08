76d76d765366e03f2c2c493c160ae40205e841b6
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2020-01-08 08:42:47 +0100

    Corrected save onnx model

--- adnlearn/adnlearn/storage/onnx.py
+++ adnlearn/adnlearn/storage/onnx.py
@@ -1,12 +1,14 @@
 import onnxmltools
 from lightgbm import LGBMClassifier
 from sklearn.linear_model import SGDClassifier
+from xgboost import XGBClassifier
 from skl2onnx import convert_sklearn
 from skl2onnx import update_registered_converter
 from skl2onnx.common.data_types import Int64TensorType, FloatTensorType, StringTensorType, DictionaryType, SequenceType
-from skl2onnx.operator_converters.RandomForest import convert_sklearn_random_forest_classifier
 from onnxmltools.convert.lightgbm.operator_converters.LightGbm import convert_lightgbm
+from onnxmltools.convert.xgboost.operator_converters.XGBoost import convert_xgboost
 from onnxmltools.convert.common.shape_calculator import calculate_linear_classifier_output_shapes
+from onnxmltools.convert.xgboost.shape_calculators.Classifier import calculate_xgboost_classifier_output_shapes
 
 def _register_converters():
     onnxmltools.convert.common.data_types.Int64TensorType = Int64TensorType
@@ -15,13 +17,13 @@ def _register_converters():
     onnxmltools.convert.common.data_types.DictionaryType = DictionaryType
     onnxmltools.convert.common.data_types.SequenceType = SequenceType
 
-    update_registered_converter(SGDClassifier, 'SklearnLinearClassifier',
-                                calculate_linear_classifier_output_shapes,
-                                convert_sklearn_random_forest_classifier)
-
     update_registered_converter(LGBMClassifier, 'LightGbmLGBMClassifier',
                                 calculate_linear_classifier_output_shapes,
                                 convert_lightgbm)
+
+    update_registered_converter(XGBClassifier, 'XGBClassifier',
+                                calculate_xgboost_classifier_output_shapes,
+                                convert_xgboost)
 
 
 def _convert_dataframe_schema(df, drop=None):
@@ -42,7 +44,7 @@ def _convert_dataframe_schema(df, drop=None):
 def save_model_onnx(model, df, filename):
     _register_converters()
     inputs = _convert_dataframe_schema(df)
-    onx = convert_sklearn(model, initial_types=inputs)
-    
+    model_onnx = convert_sklearn(model, initial_types=inputs)
+
     with open(filename, "wb") as f:
-        f.write(onx.SerializeToString())
+        f.write(model_onnx.SerializeToString())
