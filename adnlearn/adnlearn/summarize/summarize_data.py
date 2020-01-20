796bda44cd5957118178d68dec07bb2a511d569d
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2020-01-20 11:00:33 +0100

    Corrected summarize data

--- adnlearn/adnlearn/summarize/summarize_data.py
+++ adnlearn/adnlearn/summarize/summarize_data.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 import seaborn
 
@@ -8,26 +9,26 @@ def descriptive_statistics(dataset, target):
     # shape
     print("**** Shape ****")
     print(dataset.shape)
-    
+
     # types
     print("**** Types ****")
     print(dataset.dtypes)
-    
+
     # descriptions
     print("**** Descriptions ****")
     description = dataset.describe()
     print(description)
-    
+
     # correlation
     print("**** Correlations ****")
     correlations = dataset.corr(method='pearson')
     print(correlations)
-    
+
     # class distribution
     print("**** Class distribution ****")
     error_counts = dataset.groupby(target).size()
     print(error_counts)
-    
+
     # skew
     print("**** Skew ****")
     skew = dataset.skew()
@@ -35,18 +36,20 @@ def descriptive_statistics(dataset, target):
 
 
 def data_visualizations(dataset, target):
+    heightDs = math.ceil(math.sqrt(len(dataset.columns)))
+
     # histograms
     dataset.hist()
     plt.show()
-    
+
     # density
-    dataset.plot(kind='density', subplots=True, layout=(3,3), sharex=False)
+    dataset.plot(kind='density', subplots=True, layout=(heightDs,heightDs), sharex=False)
     plt.show()
-    
+
     # box and whisker plots
-    dataset.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+    dataset.plot(kind='box', subplots=True, layout=(heightDs,heightDs), sharex=False, sharey=False)
     plt.show()
 
     # scatter plot matrix
-    seaborn.pairplot(dataset, hue=target, size=3, diag_kind="kde")
+    seaborn.pairplot(dataset, hue=target, height=heightDs, diag_kind="kde")
     plt.show()
